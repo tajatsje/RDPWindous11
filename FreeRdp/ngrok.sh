5e6419e68d7879c7e74f1953deec083cb33bed9c
Taha 7x <78062230+tajatsje@users.noreply.github.com>
2024-02-09 09:29:39 +0200

    Create ngrok.sh

---
+++ FreeRdp/ngrok.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+stty intr ""
+stty quit ""
+stty susp undef
+
+clear
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Downloading ngrok..."
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
